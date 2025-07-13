f63085f5304e33d3f86788a5ab9bafba9407c056
Machelli <valpraiz81@gmail.com>
2025-07-13 14:36:48 -0700

    second argument

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+let landmass = function (...args) {
+  if (args.length == 0) { console.log("No argument")}
+  else if(args.length = 1) { console.log("Argument found")}
+ else {console.log (console.log("Argument found"))}
+}
+
+landmass ()
+landmass ("best")
+landmass ("best","school")
